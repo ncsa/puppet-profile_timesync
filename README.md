128d5a29370bc0ca1abca72b67c932da5163d7e4
Peter Enstrom <enstrom@illinois.edu>
2021-03-25 13:51:41 -0500

    Added CI for ASDPLNG-44
    Set default queryhosts to 141.142.0.0/16 instead of ncsa.illinois.edu
    
    init.pp now contains code for both client and server

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-# profile_chrony
+# profile_timesync
  
 Configures chronyd as a client or server
+
+[![pdk-validate](https://github.com/ncsa/puppet-profile_timesync/actions/workflows/pdk-validate.yml/badge.svg)](https://github.com/ncsa/puppet-profile_timesync/actions/workflows/pdk-validate.yml)
+ [![yamllint](https://github.com/ncsa/puppet-profile_timesync/actions/workflows/yamllint.yml/badge.svg)](https://github.com/ncsa/puppet-profile_timesync/actions/workflows/yamllint.yml)
  
 ## Dependencies
 - [puppet/chrony](https://github.com/voxpupuli/puppet-chrony)
  
 ## Reference
 
-### class profile_chrony (
+### class profile_timesync (
 -  Numeric $makestep_seconds,
 -  Integer $makestep_updates,
-
-### class profile_chrony::server (
+### class profile_timesync::client (
+-  Variant[Hash,Array[Stdlib::Host]] $servers,
+### class profile_timesync::server (
 -  Variant[Hash,Array[Stdlib::Host]] $servers,
 -  Array[Stdlib::IP::Address::V4] $queryhosts,
-### class profile_chrony::client (
--  Variant[Hash,Array[Stdlib::Host]] $servers,
 
 [REFERENCE.md](REFERENCE.md)
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,17 @@
 # @summary Configures Chrony as an NTP Client or Server
 #
-# This profile conifures chronyd as an NTP client or server
+# This profile configures chronyd as an NTP client or server
 #
 # @example
-#   include profile_chrony
+#   include profile_timesync
+#
+# @param servers
+#   Array of hostnames or addresses for upstream NTP servers
+#
+# @param queryhosts
+#   Array of subnets allowed to query this server via NTP
+#   Format can be a single IP or CIDR subnet
+#   Empty by default, if specified turns the machine into a Chrony server.
 #
 # @param makestep_seconds
 #   Maximum clock skew before steping clock sync
@@ -13,8 +21,30 @@
 #
 # @see https://chrony.tuxfamily.org/
 #
-class profile_chrony (
+class profile_timesync (
+  Variant[Hash,Array[Stdlib::Host]] $servers,
+  Array[Stdlib::Host] $queryhosts,
   Numeric $makestep_seconds,
   Integer $makestep_updates,
 ) {
+
+  $step_sec = lookup('profile_timesync::makestep_seconds')
+  $step_upd = lookup('profile_timesync::makestep_updates')
+
+  class { '::chrony':
+    servers          => $servers,
+    makestep_seconds => $step_sec,
+    makestep_updates => $step_upd,
+    queryhosts       => $queryhosts,
+  }
+
+  # Allow NTP incoming queries to 123/udp
+  $queryhosts.each | String $cidr | {
+    firewall { "500 profile_timesync::server - ntp from '${cidr}'" :
+      proto  => 'udp',
+      dport  => '123',
+      action => 'accept',
+      source => $cidr,
+    }
+  }
 }
